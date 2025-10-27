268da97acb5fcf05617a049633300193500ff37c
Nazim Ayub <nazimayub786@gmail.com>
2025-10-27 03:19:36 +0000

    could you make it so that the landing page is centered on adaptable size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ export default function LandingPage() {
             </div>
           </div>
         </section>
-        <section id="features" className="w-full py-12 md:py-24 lg:py-32">
+        <section id="features" className="w-full py-12 md:py-24 lg:py-32 bg-background">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
